49fc115c4690a4ee81ff83691ccf0d30d36491b5
Minh Tran <trumtalon296@MacBook-Air-cua-Minh.local>
2023-08-03 23:45:58 +0700

    feat: create landing page

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -27,24 +27,24 @@ const Navbar = () => {
 
 
     return (
-        <div className={navbar ? " ease-in duration-300 fixed w-screen bg-black" : "ease-in duration-200 fixed w-screen bg-red-500"}>
+        <div className={navbar ? " z-10 ease-in duration-300 fixed w-screen bg-[#c8cedc] opacity-90" : "z-10 ease-in duration-200 fixed w-screen bg-transparent"}>
             <nav className="sticky">
                 <div className="flex flex-wrap items-center justify-between mx-auto p-4 text-white">
-                    <img src="/divslogo.png" alt="divs team logo" className=" object-contain h-20 -m-6 pl-8" />
+                    <img src="/Home_pic.png" alt="divs team logo" className=" object-contain h-16 -m-6 pl-8" />
 
                     <div className=" mr-10">
                         <ul className="flex flex-row space-x-6">
-                            <li className=" rounded-3xl text-white hover:text-black hover:bg-yellow-300 duration-75 py-2 cursor-pointer"
+                            <li className=" rounded-3xl text-black hover:text-white hover:bg-[#4481EE] duration-200 py-2 cursor-pointer"
                                 onClick={console.log("Home")}>
                                 <Link className="  px-6 py-2" href="#home" scroll={true}>Home</Link>
                             </li>
 
-                            <li className=" rounded-3xl text-white hover:text-black hover:bg-yellow-300 duration-75 py-2 cursor-pointer"
+                            <li className=" rounded-3xl text-black hover:text-white hover:bg-[#4481EE] duration-200 py-2 cursor-pointer"
                                 onClick={console.log("About")}>
                                 <Link className="px-6 py-2" href="#about" scroll={true}>About</Link>
                             </li>
 
-                            <li className=" rounded-3xl text-white hover:text-black hover:bg-yellow-300 duration-75 py-2 cursor-pointer"
+                            <li className=" rounded-3xl text-black hover:text-white hover:bg-[#4481EE] duration-200 py-2 cursor-pointer"
                                 onClick={console.log("Work")}>
                                 <Link className="px-6 py-2" href="#work" scroll={true}>Work</Link>
                             </li>
