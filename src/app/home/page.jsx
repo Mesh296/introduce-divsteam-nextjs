7a2ed7248e5c4fdebf6e9187dbe882b52b5e4a1c
vuitinhvl7x <xuanhai2708@gmail.com>
2023-08-06 18:11:12 +0700

    feat: footer, AnimatedText.jsx

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,3 +1,4 @@
+import AnimatedText from "@/components/AnimatedText";
 import Image from "next/image";
 import { IoIosArrowForward } from 'react-icons/io';
 
@@ -5,19 +6,19 @@ const Home = () => {
     return (
         <div className=" h-screen bg-white relative" id="home">
             <div className=" text-7xl font-extrabold absolute left-32 top-[18rem] z-10">
-                <h1>LOREM</h1>
+                <AnimatedText text='LOREM' />
                 <div className=" text-base font-extralight absolute w-[30rem]">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
                 </div>
             </div>
             <div className="flex justify-between absolute cursor-pointer text-white px-6 py-2 rounded-lg bg-[#4481EE] hover:bg-[#376bc6] duration-200 left-32 top-[30rem] z-10">
-                <button 
+                <button
                     className=" pr-2 font-semibold"
                     onClick={console.log("btn clicked")}>
                     BUTTON
                 </button>
-                <IoIosArrowForward className=" m-auto h-full"/>
-            </div>      
+                <IoIosArrowForward className=" m-auto h-full" />
+            </div>
 
             <div className=" h-[30rem] w-[30rem] absolute left-48 top-1/4 ">
                 <Image
